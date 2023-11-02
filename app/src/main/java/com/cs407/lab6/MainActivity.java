30479f862a3471915d55b5307ec5373857a7205c
Pinzhe Liu <pliu227@wisc.edu>
2023-11-02 14:54:36 -0500

    milestone 1 finished

--- app/src/main/java/com/cs407/lab6/MainActivity.java
+++ app/src/main/java/com/cs407/lab6/MainActivity.java
@@ -1,20 +1,34 @@
 package com.cs407.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
+    // somewhere in Australia
+    private final LatLng mDestinationLatLng = new LatLng(43.07546, -89.40342);
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment)
+        SupportMapFragment mapFragment = (SupportMapFragment)
                         getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            //code to display marker
+            mMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
 
 
     }
